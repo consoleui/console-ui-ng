c0a57d98ccc50c96c539bb71937b8ce74b1e163e
jiawei <jiawei@parim.net>
2017-11-04 22:14:34 +0800

    完善图片裁剪demo

--- src/app/showcase/cropper-showcase/image-crop-demo/image-crop-demo.component.ts
+++ src/app/showcase/cropper-showcase/image-crop-demo/image-crop-demo.component.ts
@@ -37,9 +37,6 @@ export class ImageCropDemoComponent implements OnInit {
     },
     crop: (e) => {
       let data = e.detail;
-
-      console.log(e.type);
-      console.log("data", data);
       this.dataX = Math.round(data.x);
       this.dataY = Math.round(data.y);
       this.dataHeight = Math.round(data.height);
@@ -53,7 +50,7 @@ export class ImageCropDemoComponent implements OnInit {
     }
   };
 
-  export(event) {
+  origin(event) {
     this.cropper = event;
   }
   constructor() { }
@@ -61,6 +58,9 @@ export class ImageCropDemoComponent implements OnInit {
   ngOnInit() {
   }
 
+  cropImage() {
+    
+  }
   setDragMode(mode) {
     this.cropper.setDragMode(mode);
   }
