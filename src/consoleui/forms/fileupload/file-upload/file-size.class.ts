b94816afcfcda04165482cd6368ea3abcec6b44d
Tony <tonyeen@foxmail.com>
2018-03-01 19:16:36 +0800

    文件上传组件添加文件大小和文件类型过滤

---
+++ src/consoleui/forms/fileupload/file-upload/file-size.class.ts
@@ -0,0 +1,28 @@
+export class FileSize {
+  public static prettySize(value: any): any {
+    if (typeof value != 'number') {
+        return value;
+    }
+
+    let size = value;
+    let units = 'B';
+    if (size / 1024 > 1) {
+        size = size / 1024;
+        units = 'KB';
+    }
+    if (size / 1024 > 1) {
+        size = size / 1024;
+        units = 'MB';
+    }
+    if (size / 1024 > 1) {
+        size = size / 1024;
+        units = 'GB';
+    }
+    if (size / 1024 > 1) {
+        size = size / 1024;
+        units = 'TB';
+    }
+
+    return size.toFixed(2) + units;
+}
+}
