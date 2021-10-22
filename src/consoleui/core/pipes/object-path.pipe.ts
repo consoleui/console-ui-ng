b8eea05c8b8aa918e0f48dddc8612b8087312248
TonyEen <tonyeen@163.com>
2021-10-22 17:45:41 +0800

    table列属性值获取改支持function

--- src/consoleui/core/pipes/object-path.pipe.ts
+++ src/consoleui/core/pipes/object-path.pipe.ts
@@ -8,6 +8,10 @@ import * as ObjectPath from 'object-path';
 export class ObjectPathPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
+    if ('function' == typeof args[0] ) {
+      return args[0](value);
+    }
+
     return ObjectPath.get(value, args, '');
   }
 
